d8ef7008eb8bec575edaaeb9a7beaac41fef8862
Brendan Forster <brendan@github.com>
2016-01-04 21:03:34 +1030

    bubble up this error

--- index.js
+++ index.js
@@ -148,6 +148,8 @@ app.get('/issues/count', function(request, response) {
             console.log('Error found in response');
             console.log('Status Code: ' + err.statusCode);
             console.log('Response: ' + err.response.body);
+
+            response.status(500).send("something happened");
           }
       );
     } else {
